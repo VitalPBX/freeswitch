206e0e49e58dca7dfedaa804f2c56c877217909c
rodrigocuadra <30705964+rodrigocuadra@users.noreply.github.com>
2025-03-11 08:17:36 -0600

    Create dialplna.lua

---
+++ lua/dialplan/dialplna.lua
@@ -0,0 +1,97 @@
+--[[
+    handler.lua (dialplan)
+    Handles FreeSWITCH dialplan requests (e.g., call routing).
+--]]
+
+-- Load PostgreSQL LuaSQL library
+local luasql = require "luasql.postgres"
+
+-- Log script execution
+freeswitch.consoleLog("NOTICE", "xml_handlers/dialplan/handler.lua called\n")
+
+-- Database connection
+local env = assert(luasql.postgres())
+local conn = assert(env:connect("ring2all", "ring2all", "ring2all", "localhost", 5432))
+
+-- Extract context and destination from request parameters
+local context = params:getHeader("Hunt-Context") or params:getHeader("Caller-Context") or "default"
+local destination = params:getHeader("Caller-Destination-Number") or ""
+
+freeswitch.consoleLog("DEBUG", "Context: " .. context .. "\n")
+freeswitch.consoleLog("DEBUG", "Destination: " .. destination .. "\n")
+
+-- Query to fetch dialplan data
+local query = string.format([[
+    SELECT dc.context_name, de.extension_name, de.continue, 
+           dc2.field, dc2.expression, dc2.break_on_match, dc2.condition_order,
+           da.action_type, da.application, da.data, da.action_order
+    FROM public.dialplan_contexts dc
+    JOIN public.dialplan_extensions de ON dc.context_uuid = de.context_uuid
+    JOIN public.dialplan_conditions dc2 ON de.extension_uuid = dc2.extension_uuid
+    JOIN public.dialplan_actions da ON dc2.condition_uuid = da.action_uuid
+    WHERE dc.context_name = '%s'
+    ORDER BY de.priority, dc2.condition_order, da.action_order
+]], context)
+
+freeswitch.consoleLog("DEBUG", "SQL query: " .. query .. "\n")
+
+-- Execute query and build XML
+local cur = assert(conn:execute(query))
+local xml = [[<?xml version="1.0" encoding="utf-8"?>
+<document type="freeswitch/xml">
+  <section name="dialplan" description="Dynamic Dialplan">
+    <context name="]] .. context .. [[">]]
+
+local current_ext_name = nil
+local current_cond_order = nil
+local row = cur:fetch({}, "a")
+
+while row do
+    if current_ext_name ~= row.extension_name then
+        if current_ext_name then
+            xml = xml .. [[
+      </extension>]]
+        end
+        xml = xml .. [[
+      <extension name="]] .. row.extension_name .. [[" continue="]] .. (row.continue == "t" and "true" or "false") .. [[">]]
+        current_ext_name = row.extension_name
+        current_cond_order = nil
+    end
+
+    if current_cond_order ~= row.condition_order then
+        if current_cond_order then
+            xml = xml .. [[
+        </condition>]]
+        end
+        xml = xml .. [[
+        <condition field="]] .. row.field .. [[" expression="]] .. row.expression .. [[" break="]] .. row.break_on_match .. [[">]]
+        current_cond_order = row.condition_order
+    end
+
+    xml = xml .. [[
+          <]] .. row.action_type .. [[ application="]] .. row.application .. [[" data="]] .. row.data .. [["/>]]
+
+    row = cur:fetch({}, "a")
+end
+
+if current_cond_order then
+    xml = xml .. [[
+        </condition>]]
+end
+if current_ext_name then
+    xml = xml .. [[
+      </extension>]]
+end
+
+xml = xml .. [[
+    </context>
+  </section>
+</document>]]
+
+freeswitch.consoleLog("DEBUG", "Generated XML: " .. xml .. "\n")
+XML_STRING = xml
+
+-- Close database connections
+cur:close()
+conn:close()
+env:close()
