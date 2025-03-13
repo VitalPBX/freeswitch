a6c122501c256586b7fc38423e5b2c1834e19161
rodrigocuadra <30705964+rodrigocuadra@users.noreply.github.com>
2025-03-13 16:53:56 -0600

    Update main.lua

--- lua/main.lua
+++ lua/main.lua
@@ -25,51 +25,15 @@ end
 -- Log an info message indicating the script is running
 log("INFO", "Main.lua is handling XML request for section: " .. section .. ", source: " .. source)
 
--- Check if the source is "xml_handlers" to route to specific handler scripts
-if source == "xml_handlers" then
-    if section == "directory" then
-        -- Load the SIP registration handler script and pass settings as an argument
-        local sip_register = dofile("/usr/share/freeswitch/scripts/xml_handlers/directory/sip_register.lua")
-        sip_register(settings)
-    elseif section == "dialplan" then
-        -- Load the dialplan handler script and pass settings as an argument
-        local dialplan = dofile("/usr/share/freeswitch/scripts/xml_handlers/dialplan/dialplan.lua")
-        dialplan(settings)
-    elseif section == "configuration" then
-        -- Check if the configuration request is for sofia.conf
-        local config_name = XML_REQUEST["key_value"]
-        log("DEBUG", "Configuration name: " .. (config_name or "unknown"))
-        if config_name == "sofia.conf" then
-            local sofia_profiles = dofile("/usr/share/freeswitch/scripts/xml_handlers/sip_profiles/sip_profiles.lua")
-            if type(sofia_profiles) == "function" then
-                sofia_profiles(settings)
-            else
-                log("ERR", "sip_profiles.lua did not return a function")
-            end
-        elseif config_name == "spandsp.conf" then
-            -- Future handler for spandsp.conf
-            log("INFO", "spandsp.conf handler not implemented yet")
-            XML_STRING = '<?xml version="1.0" encoding="utf-8"?><document type="freeswitch/xml"><section name="configuration"></section></document>'
-        elseif config_name == "loopback.conf" then
-            -- Future handler for loopback.conf
-            log("INFO", "loopback.conf handler not implemented yet")
-            XML_STRING = '<?xml version="1.0" encoding="utf-8"?><document type="freeswitch/xml"><section name="configuration"></section></document>'
-        elseif config_name == "enum.conf" then
-            -- Future handler for enum.conf
-            log("INFO", "enum.conf handler not implemented yet")
-            XML_STRING = '<?xml version="1.0" encoding="utf-8"?><document type="freeswitch/xml"><section name="configuration"></section></document>'
-        elseif config_name == "timezones.conf" then
-            -- Future handler for timezones.conf
-            log("INFO", "timezones.conf handler not implemented yet")
-            XML_STRING = '<?xml version="1.0" encoding="utf-8"?><document type="freeswitch/xml"><section name="configuration"></section></document>'
-        else
-            -- Log an error if the section is not recognized
-            log("WARNING", "No handler for configuration: " .. (config_name or "unknown"))
-            XML_STRING = '<?xml version="1.0" encoding="utf-8"?><document type="freeswitch/xml"><section name="configuration"></section></document>'
-        end
-    else
-        -- Log an error if the section is not recognized
-        log("ERR", "Unknown section: " .. section)
-        XML_STRING = '<?xml version="1.0" encoding="utf-8"?><document type="freeswitch/xml"><section name="' .. section .. '"></section></document>'
-    end
-end
+--for loop through arguments
+	arguments = "";
+	for key,value in pairs(argv) do
+		if (key > 1) then
+			arguments = arguments .. " '" .. value .. "'";
+			--freeswitch.consoleLog("notice", "[app.lua] argv["..key.."]: "..argv[key].."\n");
+		end
+	end
+
+--route the request to the application
+	--freeswitch.consoleLog("notice", "["..app_name.."]".. scripts_dir .. "/main/" .. app_name .. "/index.lua\n");
+	loadfile(scripts_dir .. "/main/" .. app_name .. "/index.lua")(argv);
