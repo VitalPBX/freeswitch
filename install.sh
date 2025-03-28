c25d76714820226ca193c805d3485f0eaae78126
rodrigocuadra <30705964+rodrigocuadra@users.noreply.github.com>
2025-03-28 09:29:34 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -518,6 +518,7 @@ echo -e "*     Update Priority in Dialplna for acknowledge_call.    *"
 echo -e "************************************************************"
 sudo -u postgres psql ring2all -c "UPDATE core.dialplan_extensions SET priority = 9999 WHERE name = 'acknowledge_call';"
 UPDATE core.dialplan_extensions SET priority = 9998 WHERE name = 'enum';
+UPDATE core.dialplan_extensions SET priority = 9999 WHERE name = 'hold_music';
 
 # Restart Freeswitch Service
 echo -e "************************************************************"
